45979b05843bf31a15e8f4bc5c524738679b2779
vishymails <vishymails@gmail.com>
2021-03-19 13:56:00 +0530

    day 5

---
+++ config-consumer/src/main/java/com/ibm/configconsumer/configconsumer/ConfigConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.ibm.configconsumer.configconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigConsumerApplication.class, args);
+    }
+
+}
+
---
+++ config-consumer/src/main/java/com/ibm/configconsumer/configconsumer/ProfileController.java
@@ -0,0 +1,28 @@
+package com.ibm.configconsumer.configconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.configconsumer.configconsumer.model.MemberProfileConfiguration;
+
+@Controller
+public class ProfileController {
+
+
+    @Autowired
+    MemberProfileConfiguration memberProfileConfiguration;
+
+
+    @RequestMapping("/profile")
+    public String getConfig(Model model){
+
+        model.addAttribute("model",memberProfileConfiguration.getDefaultModel());
+        model.addAttribute("min",memberProfileConfiguration.getMinRentPeriod());
+        return "mprofile";
+
+    }
+
+}
